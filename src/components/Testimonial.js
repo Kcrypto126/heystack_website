f59f5f9d5cc3faeeb7ba756ccbe3f688cd3de0eb
Akleem khan <akleemkhan0208@gmail.com>
2024-01-14 22:36:43 +0530

    commit

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -3,31 +3,19 @@ import TestimonialCard from "./TestimonialCard";
 
 function Testimonial() {
   return (
-    <div className="md:mt-28 mt-20 testimonial testimonial">
+    <div className="mt-20 md:mt-28 testimonial">
       <div>
         <div className="text-center">
           <p className="text-center md:text-[2.5rem] text-[1.5rem] px-3 heading">
             Testimonials
           </p>
-          <p className="md:text-2xl text-base text-gray-600">
+          <p className="text-base text-gray-600 md:text-2xl">
             What peoaple say about us
           </p>
         </div>
         <div className="w-[90%] mx-auto ">
           <TestimonialCard />
         </div>
-      </div>
-      <div className="bg-[#e5ecff] md:pt-10 pt-5 flex justify-center flex-col md:mt-28 mt-20">
-        <p className="text-center md:text-[2.5rem] text-[1.5rem] px-3 heading ">
-          How HeyStack Works
-        </p>
-        <Image
-          src="/howwork.svg"
-          width={"100"}
-          height={"100"}
-          alt="work"
-          className="w-[100%] mt-4"
-        />
       </div>
     </div>
   );
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,18 +1,17 @@
 import work from "@/constants/work";
 import Image from "next/image";
-import React from "react";
 
 function Work() {
   return (
     <div className="work">
-      <div className="md:mt-26 mt-20 container mx-auto ">
+      <div className="container mx-auto mt-20 md:mt-26">
         <div className="px-1">
           <p className="text-center md:text-[2.5rem] text-[1.5rem] px-3 work-heading">
             How brands leverage Heystack Insights
           </p>
         </div>
-        <div className="md:mt-8 w-[90%] mx-auto flex flex-col gap-10 ">
-          {work.map((items, ind) => (
+        <div className="md:mt-8 w-[100%] mx-auto flex flex-col gap-10 ">
+          {work.map((item, ind) => (
             <div
               className={`flex 
             ${
@@ -21,22 +20,20 @@ function Work() {
               key={ind}
             >
               <div className="flex flex-col basis-[60%] content-div md:gap-4 gap-3 md:p-10 md:mt-0 mt-5">
-                <h1 className="md:text-2xl  tracking-wider">
+                <h1 className="tracking-wider md:text-2xl">
                   MINIMIZE OPTAMIZATION
                 </h1>
-                <p className="md:text-base text-sm text-gray-800">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  aliquet scelerisque eros, sed tempor velit consectetur in.
-                  Morbi tincidunt
+                <p className="text-sm text-gray-800 md:text-base">
+                  {item.content}
                 </p>
-                <span className="h-[0.5px] w-[90%] bg-[#464646]"></span>
+                <span className="h-[0.5px] w-[90%]  bg-gray-300"></span>
 
                 <Image
                   src={"/arrow.svg"}
                   width="200"
                   height="100"
                   alt="btn"
-                  className=" cursor-pointer"
+                  className="cursor-pointer "
                 />
               </div>
               <div className="basis-[60%] md:mt-0 mt-10 flex justify-center items-center md:p-10">

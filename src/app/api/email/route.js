5bc489d0947066f9ac98ea0cd53ed582904a9b09
Shailendra_Singh <singhshailendra1701@gmail.com>
2024-01-27 01:19:36 +0530

    hotfix

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -16,21 +16,22 @@ export async function POST(req) {
     message: `We have published a new blog on ${title} kindly check this at https://hey-stack.netlify.app/blogs/${slug}`,
   };
 
-  try {
-    const message = await fetch(
-      "https://us.locogram.com/Contactus/email.json",
-      {
-        method: "POST",
+  // try {
+  //   const message = await fetch(
+  //     "https://us.locogram.com/Contactus/email.json",
+  //     {
+  //       method: "POST",
 
-        body: JSON.stringify(EmailMessage),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }
-    ).then((response) => response.json());
-    return NextResponse.json(message, { status: 200 });
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json(err);
-  }
+  //       body: JSON.stringify(EmailMessage),
+  //       headers: {
+  //         "Content-type": "application/json; charset=UTF-8",
+  //       },
+  //     }
+  //   ).then((response) => response.json());
+  //   return NextResponse.json(message, { status: 200 });
+  // } catch (err) {
+  //   console.log(err);
+  //   return NextResponse.json(err);
+  // }
+  return NextResponse.json(EmailMessage, { status: 200 });
 }
